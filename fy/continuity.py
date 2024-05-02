d1b5ccb60a071b2aa480d23d3862ce6ddccf6669
maple5717 <momiji5717@gmail.com>
2024-05-02 20:33:22 +0800

    supported rendering with two cameras

--- fy/continuity.py
+++ fy/continuity.py
@@ -12,7 +12,14 @@ import kubric as kb
 from utils import align_can_objs, spherical_to_cartesian
 
 # TODO: test if obj is on the table at most of time; why velocity changes after teletransportation
-
+invalid_small_objs = ["02946921/f8fd565d00a7a6f9fef1ca8c5a3d2e08", 
+                      "02946921/343287cd508a798d38df439574e01b2", 
+                      "02946921/91a524cc9c9be4872999f92861cdea7a",
+                      "02946921/4d4fc73864844dad1ceb7b8cc3792fd", 
+                      "02946921/19fa6044dd31aa8e9487fa707cec1558",
+                      "02946921/129880fda38f3f2ba1ab68e159bfb347",
+                      "02946921/7b643c8136a720d9db4a36333be9155",
+                      ]
 
 class ContinuityTestScene(BaseTestScene):
     """Test scene for permanance violation.
@@ -26,17 +33,18 @@ class ContinuityTestScene(BaseTestScene):
     
     def __init__(self, FLAGS) -> None:
         super().__init__(FLAGS)
+        self.is_add_background_static_objects = False
         self.frame_violation_start = -1
         # two cases:
         # 1. object disappears
         # 0. object teleports
-        self.violation_type = np.random.binomial(n=1,p=0.5)
+        self.violation_type = 1#np.random.binomial(n=1,p=0.5)
         self.is_move_camera = np.random.binomial(n=1,p=0.5)
         self.gravity = (0, 0, -4.9)
 
         self.back_camera_pos = spherical_to_cartesian(r_range=[2.25, 2.75], theta_range=[70, 80], phi_range=[-45+180, 45+180])
-        self.default_camera_pos = spherical_to_cartesian(r_range=[2.25, 2.75], theta_range=[75, 85])
-        self.camera_look_at = (0, 0, self.ref_h)
+        self.alternative_camera_pos = spherical_to_cartesian(r_range=[2.25, 2.75], theta_range=[75, 85])
+        
 
         # if not(self.violation_type):
         #     self.default_camera_pos = spherical_to_cartesian(r_range=[2, 3], theta_range=[75, 85])
@@ -51,6 +59,10 @@ class ContinuityTestScene(BaseTestScene):
     def prepare_scene(self):
         print("preparing scene ...")
         super().prepare_scene()
+        self.alternative_camera_pos[2] += self.ref_h
+        self.default_camera_pos[2] += self.ref_h
+        self.camera_look_at = (0, 0, self.ref_h)
+        self.alternative_camera_look_at = (0, 0, self.ref_h)
 
         if not(self.violation_type):
             # remove block object
@@ -134,8 +146,9 @@ class ContinuityTestScene(BaseTestScene):
                 self.block_obj.position = (0, 0, -10)
             # initialize the obj. Note the the initial position should be (0,0,0) 
             # so that the dist between its CoM and the table surface can be calculated
-            small_obj_id = [name for name, spec in shapenet_assets._assets.items()
+            small_obj_id_all = [name for name, spec in shapenet_assets._assets.items()
                     if spec["metadata"]["category"] == "can"]
+            small_obj_id = [id for id in small_obj_id_all if id not in invalid_small_objs]
             small_obj_id = self.rng.choice(small_obj_id)
             small_obj = self.add_object(asset_id=small_obj_id,
                                     position=(0, 0, 0),
@@ -192,8 +205,8 @@ class ContinuityTestScene(BaseTestScene):
         obj = self.test_obj[0] # work for only one test obj
         for i, frame in enumerate((range(self.flags.frame_start, self.flags.frame_end))):
             bpy.context.scene.frame_set(frame)
-            vis_obj = getVisibleVertexFraction("small_obj", self.rng, self.active_camera)
-            vis_table = isPointVisible([0, 0, self.ref_h], [self.table_name, self.block_name, "small_obj"], self.active_camera)
+            vis_obj = getVisibleVertexFraction("small_obj", self.rng, "camera")
+            vis_table = isPointVisible([0, 0, self.ref_h], [self.table_name, self.block_name, "small_obj"], "camera")
             visibility_obj[i] = vis_obj  
             visibility_table[i] = vis_table
             obj_on_table[i] = obj.keyframes["position"][frame][2] > self.ref_h-0.05
@@ -211,7 +224,11 @@ class ContinuityTestScene(BaseTestScene):
             # cond_2 = visibility_obj[0] >= 0.15 \
             #         and visibility_obj[5] >= 0.15 \
             #         and visibility_obj[-6] >= 0.15
-            cond_2 = is_obj_visible[:6].max() and is_obj_visible[-7:].max()
+            if less_strict:
+                cond_2 = is_obj_visible[:10].max() and is_obj_visible[-10:].max()
+            else:
+                cond_2 = is_obj_visible[:6].max() and is_obj_visible[-7:].max()
+            
             cond = [cond_2,
                     cond_1 or less_strict,
                     in_view[0],
@@ -220,6 +237,7 @@ class ContinuityTestScene(BaseTestScene):
                     is_table_visible[6:-6].min(), 
                     obj_on_table.sum() >= 16  or less_strict
                     ]
+            
         else:
             cond = [visibility_obj[0] >= 0.5,
                     in_view[0],
@@ -229,6 +247,10 @@ class ContinuityTestScene(BaseTestScene):
                     ]
 
         is_valid = np.min(cond)
+        logging.error(cond)
+        # logging.error(np.min(cond))
+        # logging.error(is_obj_visible[:10])
+        # logging.error( is_obj_visible[-10:])
         if is_valid:
             # set when the test object is set disappeared  
             if self.violation_type:
